d3e500965876e136608ba35b4d37e4b90d4d8287
Alena Izmailova <a.a.i@lenta.ru>
2023-05-21 04:12:16 +0300

    Add orderingwithoutauthorization.cy.js

---
+++ cypress/e2e/orderingwithoutauthorization.cy.js
@@ -0,0 +1,25 @@
+describe('Ordering without authorization', () => {
+    it('Adding the product to the cart', () => {
+// Goes to the homepage
+      cy.visit('https://www.demoblaze.com')
+// Open product menu
+      cy.get('#itemc').click()
+// Add an item to the cart
+      cy.get('#tbodyid > div:nth-child(5) > div > div > h4 > a').click()
+      cy.get('#tbodyid > div.row > div > a').click()
+// Goes to the Cart
+      cy.get('#cartur').click()
+// Make an order
+      cy.get('#page-wrapper > div > div.col-lg-1 > button').click()  
+// Fill out fields
+      cy.get('#name').type('TestName')
+      cy.get('#country').type('TestCountry')
+      cy.get('#city').type('TestCity')
+      cy.get('#card').type('1234567887654321')
+      cy.get('#month').type('May')
+      cy.get('#year').type('2023')
+// Click Purchase button
+      cy.get ('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+      cy.get('.confirm').click()
+    })
+  })
