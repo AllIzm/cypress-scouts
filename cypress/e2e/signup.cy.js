39287296b76d255c7025b0937e4805bf0dfcf7ac
Alena Izmailova <a.a.i@lenta.ru>
2023-05-28 09:37:52 +0300

    Update selector

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,9 +1,12 @@
 describe('Signing up', () => {
     it('Create New User', () => {
+
 // Goes to the homepage
       cy.visit('https://www.demoblaze.com')
+
 // Open Sign up pop-up
       cy.get('#signin2').click()
+
 // Fill out fields
 const testUsername = ('TestUser' + Date.now())
 
@@ -11,6 +14,7 @@ cy.get('#sign-username').invoke('val', testUsername)
 cy.get('#sign-password').invoke('val', 'TestPassword')
 
 // Click Sign up button
-cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+cy.get('.btn-primary').contains('Sign up').click();
     })
+
 })
