f1545e95b5c549692ee640a74a5bfa7fa4bd4d9d
Alena Izmailova <a.a.i@lenta.ru>
2023-05-31 23:38:28 +0300

    Add assertions

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -5,7 +5,10 @@ describe('Signing up', () => {
       cy.visit('https://www.demoblaze.com')
 
 // Open Sign up pop-up
-      cy.get('#signin2').click()
+      cy.get('#signin2').should('be.visible').click()
+
+// Check Sign up title      
+      cy.get('#signInModalLabel').should('have.text', 'Sign up')
 
 // Fill out fields
 const testUsername = ('TestUser' + Date.now())
@@ -13,8 +16,13 @@ const testUsername = ('TestUser' + Date.now())
 cy.get('#sign-username').invoke('val', testUsername)
 cy.get('#sign-password').invoke('val', 'TestPassword')
 
-// Click Sign up button
-cy.get('.btn-primary').contains('Sign up').click();
+// Check alert 'Sign up successful.'
+cy.window().then((win) => {
+    cy.stub(win, 'alert').as('winAlert')
+    // Click Sign up button
+    cy.get('.btn-primary').contains('Sign up').click()      
+    cy.get('@winAlert').should('be.calledWith', 'Sign up successful.')
     })
-
+  })
+  
 })
