aaeb65796cbe881d4fa049ea57a6d9badbe4b9a7
Alena Izmailova <a.a.i@lenta.ru>
2023-06-10 01:43:39 +0300

    Refactoring p1

--- cypress/e2e/ordering.cy.js
+++ cypress/e2e/ordering.cy.js
@@ -14,18 +14,18 @@ describe('Purchasing', () => {
       cy.window().then((win) => {
             cy.stub(win, 'alert').as('winAlert')
             // Add an item to the cart
-            cy.xpath('//a[@class="btn btn-success btn-lg"]').click()
+            cy.findByText('Add to cart').click() 
             cy.get('@winAlert').should('be.calledWith', 'Product added')
           })
       
 // Go to the Cart
       cy.get('#cartur').click()
 
-// Check if there is an item in the cart
-      cy.xpath('//a[contains(@onclick, "deleteItem")]').should('be.visible')
+// Check if there is one item in the cart at least
+      cy.contains('Delete').should('have.length', 1),
 
 // Make an order
-      cy.xpath('//button[contains(@class, "btn-success")]').click()
+      cy.findByRole('button', {name: /Place Order/i}).click()
 
 // Fill out fields
       cy.get('#name').invoke('val', 'TestName')
@@ -39,7 +39,7 @@ describe('Purchasing', () => {
       cy.get ('.btn-primary').contains('Purchase').click()
 
 // Check notification for success order
-      cy.xpath('//*[@class="sweet-alert  showSweetAlert visible"]').should('be.visible')
+     cy.findByText('Thank you for your purchase!').should('be.visible') 
 
 // Click "OK" button      
       cy.wait(1000)
@@ -50,7 +50,7 @@ describe('Purchasing', () => {
     
 // Check if the Cart is empty
       cy.get('#cartur').click()
-      cy.xpath('//a[contains(@onclick, "deleteItem")]').should('not.exist') 
+      cy.contains('Delete').should('not.exist') 
 
     })
 
