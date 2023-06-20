01eff85ea87265b944f09d35ab2c451a961fde4d
Alena Izmailova <a.a.i@lenta.ru>
2023-06-20 16:40:53 +0300

    test: add existing user reg test

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,31 +1,45 @@
 describe('Signing up', () => {
       beforeEach ( () => {
 // Goes to the homepage
-      cy.visit('/')
+    cy.visit('/')
 
 // Open Sign up pop-up
-cy.get('#signin2').should('be.visible').click()
+    cy.get('#signin2').should('be.visible').click()
 
 // Check Sign up title      
-      cy.get('#signInModalLabel').should('have.text', 'Sign up')
+    cy.get('#signInModalLabel').should('have.text', 'Sign up')
 }) 
 
-// Successful Registration
+// Test1 Successful Registration
     it('Success Reg via UI', () => {
 
-      const username = ('TestUser' + Date.now())
-      const password = 'TestPassword'      
+    const username = ('TestUser' + Date.now())
+    const password = 'TestPassword'      
 
 // Commands function
-      cy.mySuperRegistration(username, password)
+    cy.mySuperRegistration(username, password)
 
 // Check alert 'Sign up successful.'
-cy.window().then((win) => {
+    cy.window().then((win) => {
     cy.stub(win, 'alert').as('winAlert')
     cy.get('@winAlert').should('be.calledWith', 'Sign up successful.')
     })
   })
   
+// Test2 User-exist Registration
+    it('User-exist Reg', () => {
+
+    const username = 'TestUser'
+    const password = 'TestPassword'      
+
+// Commands function
+    cy.mySuperRegistration(username, password)
+
+// Check alert 'This user already exist.'
+    cy.window().then((win) => {
+    cy.stub(win, 'alert').as('winAlert')
+    cy.get('@winAlert').should('be.calledWith', 'This user already exist.')
+    })
+  })
+  
 })
-
-
