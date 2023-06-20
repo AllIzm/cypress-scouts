881f7c33faf05f99df072de5d8bf5f9e2f763b54
Alena Izmailova <a.a.i@lenta.ru>
2023-06-20 23:35:12 +0300

    test: add test for monitor, laptops

--- cypress/e2e/ordering.cy.js
+++ cypress/e2e/ordering.cy.js
@@ -54,4 +54,96 @@ describe('Purchasing', () => {
 
   })
 
+  // Test2
+  it('Ordering Laptops', () => {
+
+    // Open product menu "Laptops"
+    cy.get('.list-group-item').contains('Laptops').click()
+  
+    cy.wait('@category')
+        
+    // Go to the item description
+    cy.contains('.card-title', 'Dell i7 8gb').click()
+    cy.url().should('include', '/prod.htm')
+      
+    // Check alert for adding an item to the cart
+    cy.window().then((win) => {
+      cy.stub(win, 'alert').as('winAlert')
+      // Add an item to the cart
+      cy.findByText('Add to cart').click() 
+      cy.get('@winAlert').should('be.calledWith', 'Product added')
+    })
+              
+    // Go to the Cart
+    cy.get('#cartur').click()
+    cy.wait('@viewcart')
+  
+    // Check if there is one item in the cart at least
+    cy.contains('Delete').should('have.length', 1)
+  
+    // Make an order with cy.commands
+    cy.placeOrder()
+  
+    // Check notification for success order
+    cy.findByText('Thank you for your purchase!').should('be.visible') 
+  
+    // Click "OK" button      
+    cy.wait(1000)
+    cy.contains('.confirm', 'OK').click();
+  
+    // Check if main page open after purchase
+    cy.url().should('include', 'index.html')
+  
+    // Check if the Cart is empty
+    cy.get('#cartur').click()
+    cy.contains('Delete').should('not.exist') 
+  
+    })
+
+  // Test3
+  it('Ordering Monitors', () => {
+
+    // Open product menu "Monitors"
+    cy.get('.list-group-item').contains('Monitors').click()
+  
+    cy.wait('@category')
+        
+    // Go to the item description
+    cy.contains('.card-title', 'ASUS Full HD').click()
+    cy.url().should('include', '/prod.htm')
+      
+    // Check alert for adding an item to the cart
+    cy.window().then((win) => {
+      cy.stub(win, 'alert').as('winAlert')
+      // Add an item to the cart
+      cy.findByText('Add to cart').click() 
+      cy.get('@winAlert').should('be.calledWith', 'Product added')
+    })
+              
+    // Go to the Cart
+    cy.get('#cartur').click()
+    cy.wait('@viewcart')
+  
+    // Check if there is one item in the cart at least
+    cy.contains('Delete').should('have.length', 1)
+  
+    // Make an order with cy.commands
+    cy.placeOrder()
+  
+    // Check notification for success order
+    cy.findByText('Thank you for your purchase!').should('be.visible') 
+  
+    // Click "OK" button      
+    cy.wait(1000)
+    cy.contains('.confirm', 'OK').click();
+  
+    // Check if main page open after purchase
+    cy.url().should('include', 'index.html')
+  
+    // Check if the Cart is empty
+    cy.get('#cartur').click()
+    cy.contains('Delete').should('not.exist') 
+  
+    })
+
 })
